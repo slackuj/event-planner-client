6b552d956ef5e8583c7a5d581152b266172e9a06
Umesh Joshi <slackuj@gmail.com>
2026-05-21 20:07:11 +0545

    update

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,5 +1,3 @@
-export const DEFAULT_PROFILE_IMAGE = "https://res.cloudinary.com/dlfeusct3/image/upload/v1777224484/user_ke5fj7.jpg";
-
 export const REMOVE_PARTICIPANT_DIALOG_MODAL = {
     title: "Remove Participant",
     content: "You won't be able to undo this action",
@@ -49,7 +47,7 @@ const currentYear = new Date().getFullYear();
 export const DEFAULT_START_DATE = new Date(Date.UTC(currentYear, 0, 1, 0, 0, 0, 0)).getTime();
 
 // Dec 31 of current year at local 23:59:59
-export const DEFAULT_END_DATE = new Date(Date.UTC(currentYear, 11, 31, 23, 59, 59, 999)).getTime();
+export const DEFAULT_END_DATE = new Date(currentYear, 11, 31, 23, 59, 59, 999).getTime();
 
 // 1. Get the current calendar digits from the local machine
 const now = new Date();
